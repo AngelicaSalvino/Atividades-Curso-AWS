608c09d17f8206c7b6674f21bf4cfee97ed7f04a
Angélica Salvino <95454748+AngelicaSalvino@users.noreply.github.com>
2023-11-23 12:25:42 -0300

    Add files via upload

---
+++ Aula-Java/scripts.js
@@ -0,0 +1,11 @@
+
+// arquivo funcao.js
+
+function imprimirMensagem() {
+    console.log("Conexão feita com sucesso!");
+  }
+  
+  // Chama a função três vezes
+  imprimirMensagem();
+  imprimirMensagem();
+  imprimirMensagem();
